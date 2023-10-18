64c36bb644ba578107cd9423ec58fb1d58bbfeff
mohammadmu1 <mbn.1991@yahoo.com>
2023-10-18 06:39:08 +0300

    backbutton

--- app/src/main/java/com/example/myapplication/AllTaskActivity.java
+++ app/src/main/java/com/example/myapplication/AllTaskActivity.java
@@ -2,7 +2,10 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AllTaskActivity extends AppCompatActivity {
 
@@ -10,5 +13,16 @@ public class AllTaskActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_task);
+
+        Button backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent goToAllTaskActivity = new Intent(AllTaskActivity.this , MainActivity.class);
+                startActivity(goToAllTaskActivity);
+
+            }
+        });
     }
 }
